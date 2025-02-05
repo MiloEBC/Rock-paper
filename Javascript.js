998e324c29cc9f2e57ee578e8d3230e7bd64f393
Emil Bjerre Christiansen <ebch31974@edu.ucl.dk>
2025-02-05 11:56:10 +0100

    fixed logic

--- Javascript.js
+++ Javascript.js
@@ -3,12 +3,34 @@
     return choices[Math.floor(Math.random() * 3)];
 }
 function getHumanChoice () {
+  
   let choice = prompt("Chose: Rock paper or scissors").toLowerCase();
+
+  
+  if (!["rock", "paper", "scissors"].includes(choice)) {
+    console.log("Invalid choice. Please choose rock, paper, or scissors.");
+    return;
+}
 
   let computerChoice = getRandomChoice();
 
   console.log(`You chose: ${choice}`);
-  console.log(`Computer chose: `);
+  console.log(`${computerChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+
+  if(choice === computerChoice){
+    console.log("It's a draw");
+
+  }
+  else if((choice === "rock" && computerChoice === "paper") || (choice === "paper" && computerChoice === "scissors") || (choice === "scissors" && computerChoice === "rock")) {
+    console.log("you loose");
+
+
+  }
+  else{
+    console.log("you win");
+  }
+
 
 }
 
